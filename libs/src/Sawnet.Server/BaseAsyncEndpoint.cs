3ffa5069456e8ece5c1818acbb43257125af48ca
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2023-09-25 20:32:52 +0200

    Add base endpoints

---
+++ libs/src/Sawnet.Server/BaseAsyncEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sawnet.Server;
+
+public static class BaseAsyncEndpoint
+{
+    public static class WithRequest<TRequest>
+    {
+        public abstract class WithResponse<TResponse> : EndpointBase
+        {
+            public abstract Task<ActionResult<TResponse>> HandleAsync(
+                TRequest request,
+                CancellationToken cancellationToken = default (CancellationToken));
+        }
+
+        public abstract class WithoutResponse : EndpointBase
+        {
+            public abstract Task<ActionResult> HandleAsync(
+                TRequest request,
+                CancellationToken cancellationToken = default (CancellationToken));
+        }
+    }
+
+    public static class WithoutRequest
+    {
+        public abstract class WithResponse<TResponse> : EndpointBase
+        {
+            public abstract Task<ActionResult<TResponse>> HandleAsync(
+                CancellationToken cancellationToken = default (CancellationToken));
+        }
+
+        public abstract class WithoutResponse : EndpointBase
+        {
+            public abstract Task<ActionResult> HandleAsync(CancellationToken cancellationToken = default (CancellationToken));
+        }
+    }
+}
