bc7b724ae7447be00c0de80cee4f881edbffa6e1
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2023-09-25 20:30:15 +0200

    Add modules

---
+++ libs/src/Sawnet.Core/Modules/SawnetModule.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Sawnet.Core.Modules;
+
+public abstract class SawnetModule
+{
+    public abstract void ConfigureCustomServices(IServiceCollection services);
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        ConfigureCustomServices(services);
+        var modules = GetModules();
+
+        foreach (var module in modules)
+        {
+            module.ConfigureServices(services);
+        }
+    }
+
+    private IReadOnlyList<SawnetModule> GetModules()
+    {
+        var modulesToIncludeAttribute = GetType().GetCustomAttribute<ModulesToInclude>();
+
+        return modulesToIncludeAttribute?.Modules ?? Enumerable.Empty<SawnetModule>().ToList();
+    }
+}
