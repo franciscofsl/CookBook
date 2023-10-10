132edb51d3c2a5f4f11b0d73e5bb14d9d0e83102
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2023-10-10 18:37:05 +0200

    Update to use docker sql server

--- src/CookBook.Infrastructure/Data/CookBookDataModule.cs
+++ src/CookBook.Infrastructure/Data/CookBookDataModule.cs
@@ -1,5 +1,7 @@
 ﻿using CookBook.Core.Recipes;
 using CookBook.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Sawnet.Core.Modules;
 using Sawnet.Infrastructure.Data;
@@ -11,6 +13,11 @@ public class CookBookDataModule : SawnetModule
 {
     public override void ConfigureCustomServices(IServiceCollection services)
     {
+        var serviceProvider = services.BuildServiceProvider();
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<IDbContext, CookBookDbContext>(opt => opt.UseSqlServer(connectionString));
+
         services.AddTransient<IRecipesRepository, RecipesRepository>();
     }
 }
