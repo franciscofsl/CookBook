a7405a0917ae6539cbf84b02a93079bdb54f5d51
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2023-10-28 23:46:38 +0200

    Add validations to recipe publish with result

--- tests/CookBook.Core.Tests/Recipes/RecipeTest.cs
+++ tests/CookBook.Core.Tests/Recipes/RecipeTest.cs
@@ -8,13 +8,53 @@ public class RecipeTest
     [Fact]
     public void Should_Publish_Recipe()
     {
-        var recipe = RecipeBuilder.Create().Build();
-        
+        var recipe = RecipeBuilder.Create()
+            .SetTitle((RecipeTitle)"Title")
+            .SetDescription((RecipeDescription)"Description")
+            .Build();
+
+        recipe.Ingredients.AddIngredient("Milk");
+
         recipe.IsDraft.Should().BeTrue();
-        
+
         recipe.Publish();
 
         recipe.IsDraft.Should().BeFalse();
         recipe.Events.Should().Contain(_ => _ is RecipePublished);
+    }
+
+    [Fact]
+    public void Should_Not_Publish_Recipe_Without_Title()
+    {
+        var recipe = RecipeBuilder.Create().Build();
+
+        var result = recipe.Publish();
+
+        result.Error.Should().Be(RecipeErrors.NotHasTitle);
+    }
+
+    [Fact]
+    public void Should_Not_Publish_Recipe_Without_Description()
+    {
+        var recipe = RecipeBuilder.Create()
+            .SetTitle((RecipeTitle)"Title")
+            .Build();
+
+        var result = recipe.Publish();
+
+        result.Error.Should().Be(RecipeErrors.NotHasDescription);
+    }
+
+    [Fact]
+    public void Should_Not_Publish_Recipe_Without_Ingredients()
+    {
+        var recipe = RecipeBuilder.Create()
+            .SetTitle((RecipeTitle)"Title")
+            .SetDescription((RecipeDescription)"Description")
+            .Build();
+
+        var result = recipe.Publish();
+
+        result.Error.Should().Be(RecipeErrors.NotHasIngredients);
     }
 }
