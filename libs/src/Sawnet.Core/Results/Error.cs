c7e7de391834f5bcc810c9dc2024106e00e21394
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2023-12-25 22:20:06 +0100

    migrate to use nugets

--- libs/src/Sawnet.Core/Results/Error.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sawnet.Core.Results;
-
-public record class Error(string Code, string Message)
-{
-    public static readonly Error None = new(string.Empty, string.Empty);
-
-    public override string ToString() => Code;
-}
--- libs/tests/Sawnet.Core.Tests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-﻿// Global using directives
-
-global using System.Reflection;
-global using FluentAssertions;
-global using Microsoft.Extensions.DependencyInjection;
-global using Sawnet.Core.BaseTypes;
-global using Sawnet.Core.Events;
-global using Sawnet.Core.Modules;
